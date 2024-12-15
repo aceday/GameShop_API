87b11bec7b24c0b00a13f0c75d2c49e132b75f60
aceday <markcediebuday@gmail.com>
2024-12-15 08:01:00 +0800

    Login fix done

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 app.config["MYSQL_HOST"] = "192.168.1.139"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "root"
-app.config["MYSQL_DB"] = "event_reservations"
-app.config["SECRET_KEY"] = "kyle123"
+app.config["MYSQL_DB"] = "gameshop"
+app.config["SECRET_KEY"] = "marc123"
 
 mysql = MySQL(app)
 auth = HTTPBasicAuth()
@@ -30,6 +30,59 @@ def page_not_found(e):
 def hello_world():
     with open("index.html", "r") as f:
         return f.read()
+
+# SECTION: LOGIN DATA
+@app.route("/login", methods=["POST"])
+def login_post():
+    data = request.json
+
+    # Fetch username and password
+    # Validate input
+    username = data.get("username")
+    password = data.get("password")
+
+    # Validate input
+    if not username or not password:
+        return jsonify({"success": False, "message": "Username and Password Required"}), 400
+
+    # Query user data
+    cur = mysql.connection.cursor()
+    cur.execute("""SELECT 
+                        username,
+                        passwd,
+                        role_id
+                    FROM users 
+                    WHERE username = %s""",
+                    (username,))
+
+    user_auth = cur.fetchone()
+
+    # Check authentication
+    if user_auth and check_password_hash(user_auth[1], password):
+        
+        token = jwt.encode({
+            "username": username,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        }, app.config["SECRET_KEY"], algorithm="HS256")
+        
+        cur.execute("""UPDATE users
+                    SET token_id = %s
+                    WHERE username = %s""",
+                    (token, username))
+        
+        mysql.connection.commit()
+
+        return jsonify({
+            "success": True,
+            "message": "Logged In Successfully",
+            "token": token,
+            "data": user_auth
+        }), 200
+    else:
+        return jsonify({
+            "success": False,
+            "message": "Invalid Username or Password"
+        }), 401
 
 
 # SECTION: CREATE AN USER ACCOUNT
@@ -46,15 +99,18 @@ def register_post():
     password = data.get("password")
 
     if (not username) or (not password):
-        return jsonify({"success": False, "message": "Username and password are required"}, 400)
+        return jsonify({
+            "success": False,
+            "message": "Username and password are required"
+            }), 401
 
     try:
 
         cur = mysql.connection.cursor()
         cur.execute("SELECT username FROM users WHERE username = %s", (username,))
         
-        if cur.fetchone():
-            return jsonify({"success": False, "message": "Username already exists"}, 400)
+        # if cur.fetchone():
+        #     return jsonify({"success": False, "message": "Username already exists"}, 409)
 
         role_id         = 1         # Default role_id
         password_enc    = generate_password_hash(password)
@@ -63,17 +119,20 @@ def register_post():
         cur.execute("INSERT INTO users (username, passwd, role_id) VALUES (%s, %s, %s)", (username, password_enc, role_id))
         mysql.connection.commit()
         
-        return jsonify({"success": True, "message": "Account created successfully"}, 201)
+        return jsonify({
+            "success": True,
+            "message": "Account created successfully"
+            }), 201
     
     except Exception as e:
         mysql.connection.rollback()
         return jsonify({"success": False,
                         "message": str(e)
-                        },409)
+                        }), 409
     finally:
         cur.close()
 
-
+# Token
 
 
 if __name__ == "__main__":
